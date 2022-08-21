3df0737425229ee015041b98643adedd2c667b84
Aleksey <busido_na87@mail.ru>
2022-08-21 23:13:33 +0300

    task_62

---
+++ task_62/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+Console.WriteLine("Задача №62");
+int m = 4;
+int n = 4;
+int[,] matrix = new int[m, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1)){
+    matrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < matrix.GetLength(1) - 1){
+        j++;}
+    else if (i < j && i + j >= matrix.GetLength(0) - 1){
+        i++;}
+    else if (i >= j && i + j > matrix.GetLength(1) - 1){
+        j--;}
+    else{
+        i--;}}
+Console.WriteLine("Полученный массив в виде спирали:");
+PrintMatrix(matrix);
+void PrintMatrix (int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            if (array[i,j] / 10 <= 0){
+                Console.Write($" {array[i,j]} ");}
+            else{
+                Console.Write($"{array[i,j]} ");}}
+Console.WriteLine();}}
